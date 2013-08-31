12f166348626db9790c80626ea59df88f520d8ef
Pedro Nascimento <pnascimento@gmail.com>
2013-08-31 17:44:53 -0300

    main_site and to_santander actions in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def main
+
+  end
+
+  def to_santander
+    redirect_to "https://www.santandernet.com.br/default.asp?CODCONCESSIONARIA=#{params[:barcode]}"
+  end
 end
